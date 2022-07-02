002e9890ff535dfe0c23c851b0718d8624acef72
Rebecca Webb <rebeccawebb817@gmail.com>
2022-07-02 16:19:03 -0500

    sums working

--- calc.js
+++ calc.js
@@ -14,6 +14,17 @@ reader.question("What would you like to calculate?", function(input){
 	const num1 = Number(tokens[1]);
 	const num2 = Number(tokens[2]);
 
+	// console.log(tokens)
+
+	// console.log('mathSymbol', mathSymbol);
+	// console.log('num1', num1);
+	// console.log('num2', num2);
+
+	// ---------- addition -------------//
+	if (mathSymbol==="+"){
+		const sum = num1 + num2;
+		console.log("sum:",sum);
+	}
 
 	// This line closes the connection to the command line interface.
 	reader.close()
